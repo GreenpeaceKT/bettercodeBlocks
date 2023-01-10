ee079c13c3e2f2a3853e112456330e97639d8e9a
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-01-10 17:23:32 +0900

    add files

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+import com.aliucord.gradle.AliucordExtension
+import com.android.build.gradle.BaseExtension
+
+buildscript {
+    repositories {
+        // mavenLocal()
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://maven.aliucord.com/snapshots")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.aliucord:gradle:main-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.32")
+    }
+}
+
+allprojects {
+    repositories {
+        // mavenLocal()
+        google()
+        mavenCentral()
+        maven("https://maven.aliucord.com/snapshots")
+    }
+}
+
+fun Project.aliucord(configuration: AliucordExtension.() -> Unit) = extensions.getByName<AliucordExtension>("aliucord").configuration()
+
+fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+
+subprojects {
+    apply(plugin = "com.android.library")
+    apply(plugin = "com.aliucord.gradle")
+    apply(plugin = "kotlin-android")
+
+    aliucord {
+        author("GreenpeaceKT", 644526522811547659)
+        updateUrl.set("https://raw.githubusercontent.com/GreenpeaceKT/bettercodeBlocks/builds/updater.json")
+        buildUrl.set("https://raw.githubusercontent.com/GreenpeaceKT/bettercodeBlocks/builds/%s.zip")
+    }
+
+    android {
+        compileSdkVersion(30)
+
+        defaultConfig {
+            minSdk = 24
+            targetSdk = 30
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "11"
+                freeCompilerArgs = freeCompilerArgs +
+                    "-Xno-call-assertions" +
+                    "-Xno-param-assertions" +
+                    "-Xno-receiver-assertions"
+            }
+        }
+    }
+
+    dependencies {
+        val discord by configurations
+        val compileOnly by configurations
+
+        discord("com.discord:discord:126018")
+        compileOnly("com.aliucord:Aliucord:main-SNAPSHOT")
+        // compileOnly("com.github.Aliucord:Aliucord:unspecified")
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
