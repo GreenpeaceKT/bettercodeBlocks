d9d397de59edb11599d2b99b706fd1c06ed1789c
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-01-13 13:35:07 +0900

    fix

--- BetterCodeBlocks/build.gradle.kts
+++ BetterCodeBlocks/build.gradle.kts
@@ -1,11 +1,18 @@
-version = "0.1.0"
+version = "1.0.0"
 
 description = "Appear Syntax highlight!!!!!!"
 
 aliucord {
 
     changelog.set("""
-        changelog is here !!!!!
+        # v0.1.0 beta
+        * published
+        # v1.0.0
+        * released
+        * diff,md,fixを言語として追加
+        * いくつかの言語の色彩を変更
+        * 軽微な不具合を修整
+        
     """.trimIndent())
     
     
