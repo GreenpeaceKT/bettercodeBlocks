596fb12086e05d15621c190873d2a177aafac7ec
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-01-16 20:02:17 +0900

    a

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        maven("https://maven.aliucord.com/snapshots")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
