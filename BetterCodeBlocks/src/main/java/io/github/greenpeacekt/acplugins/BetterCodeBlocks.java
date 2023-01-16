1c6d9cd2ee9d068e7cea5d6b7683b2a0509c7b43
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-01-16 15:41:58 +0900

    build

--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
@@ -59,7 +59,7 @@ public final class BetterCodeBlocks extends Plugin {
             }
         }));
 
-        patcher.patch(MDUtils.class.getDeclaredMethod("renderCodeBlock", Context.class, SpannableStringBuilder.class, String.class, String.class),
+        /*patcher.patch(MDUtils.class.getDeclaredMethod("renderCodeBlock", Context.class, SpannableStringBuilder.class, String.class, String.class),
             new PreHook(param -> {
                 var lang = (String) param.args[2];
                 if (!Settings.Companion.get(settings, lang)) return;
@@ -73,7 +73,7 @@ public final class BetterCodeBlocks extends Plugin {
                 param.setResult(builder);
             })
         );
-    }
+    */}
 
     @Override
     public void stop(Context context) {
