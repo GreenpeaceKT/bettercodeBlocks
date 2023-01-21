133bec588883a348687215528a3d680feaf0ed59
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-01-21 23:27:34 +0900

    a

--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/LangNode.java
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/LangNode.java
@@ -19,28 +19,28 @@ import io.github.greenpeacekt.acplugins.BetterCodeBlocks;
 import io.github.greenpeacekt.acplugins.bettercodeblocks.*;
 
 public final class LangNode<RC extends BasicRenderContext> extends a<RC> {
-    /*private final String lang;
+    private final String lang;
 
     @SafeVarargs
     public LangNode(String lang, Node<RC>... child) {
-        super(child);
+        /*super(child);
         this.lang = lang;
         
-    }
+    */}
 
     public void render(SpannableStringBuilder builder, RC rc) {
-        if (!(builder == null || this.lang == null)) {
+        /*if (!(builder == null || this.lang == null)) {
             Utils.ensureEndsWithNewline(builder);
             renderLang(builder, rc.getContext(), this.lang, builder.length());
         }
         super.render(builder, rc);
-    }
+    */}
 
     public static void renderLang(SpannableStringBuilder builder, Context ctx, String lang, int a) {
-        if (Settings.Companion.get()) {
+        /*if (Settings.Companion.get()) {
             builder.append(lang).append("\n");
             builder.setSpan(new RelativeSizeSpan(0.85f), a, builder.length(), 33);
             Utils.fixColor(builder, ctx, a);
-        }
-    }*/
+        }*/
+    }
 }
