4a88d408b14d2692d2249bc9b0d59c66baed7c1f
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-01-21 14:54:16 +0900

    a

--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/Settings.kt
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/Settings.kt
@@ -11,31 +11,8 @@ import com.discord.views.CheckedSetting
 import com.lytefast.flexinput.R
 import io.github.greenpeacekt.acplugins.BetterCodeBlocks
 
-class Settings(private val plugin: BetterCodeBlocks) : AppBottomSheet() {
-        override fun getContentViewResId() = 0
-
-        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, bundle: Bundle?): View {
-            val context = inflater.context
-            val layout = LinearLayout(context)
-            layout.setBackgroundColor(ColorCompat.getThemedColor(context, R.b.colorBackgroundPrimary))
-
-            layout.addView(Utils.createCheckedSetting(context, CheckedSetting.ViewType.SWITCH, "Developer mode", null).apply {
-                val key = "sort"
-                isChecked = plugin.settings.getBool(key, true)
-                setOnCheckedListener {
-                    plugin.settings.setBool(key, it)
-                    if (it) plugin.sortExperiments()
-                    else {
-                        plugin.sortUnpatch?.run()
-                        plugin.sortUnpatch = null
-                    }
-                }
-            })
-            return layout
-        }
-    }
-    
-/*class Settings(private val settings: SettingsAPI) : SettingsPage() {
+ 
+class Settings(private val settings: SettingsAPI) : SettingsPage() {
     @SuppressLint("SetTextI18n")
     override fun onViewBound(view: View) {
         super.onViewBound(view)
@@ -46,31 +23,11 @@ class Settings(private val plugin: BetterCodeBlocks) : AppBottomSheet() {
         val layout = linearLayout
 
         layout.addView(com.aliucord.Utils.createCheckedSetting(context, CheckedSetting.ViewType.SWITCH, "Not supported languages", null).apply {
-            val key = "notSupported"
+            val key = "dev"
             isChecked = settings.getBool(key, false)
             setOnCheckedListener { settings.setBool(key, it) }
         })
-
-        for (entry in SWITCHES) {
-            val lang = entry.key
-            layout.addView(com.aliucord.Utils.createCheckedSetting(context, CheckedSetting.ViewType.SWITCH, lang, null).apply {
-                isChecked = settings.getBool(lang, entry.value)
-                setOnCheckedListener { settings.setBool(lang, it) }
-            })
-        }
     }
 
-    companion object {
-        private val BLACKLIST = listOf("protobuf", "proto", "pb", "rs", "rust", "cql", "cr", "crystal")
-        val SWITCHES = mapOf("js" to true, "json" to true, "go" to false)
-
-        fun get(settings: SettingsAPI, lang: String?): Boolean {
-            val notSupported = settings.getBool("notSupported", false)
-            if (lang == null) return notSupported
-            if (BLACKLIST.contains(lang)) return false
-            val realLang = GrammarLocatorImpl.realLanguageName(lang)
-            return settings.getBool(realLang, SWITCHES.getOrDefault(realLang, true)) &&
-                (notSupported || BetterCodeBlocks.grammarLocator.grammar(BetterCodeBlocks.prism4j, lang) != null)
-        }
-    }
-}*/
+    
+}
