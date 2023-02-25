ee87d5a30d1782c2d16b494e208b5e0f2d33bb66
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-02-25 16:31:42 +0900

    dev

--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/Settings.kt
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/Settings.kt
@@ -3,11 +3,13 @@ package io.github.greenpeacekt.acplugins.bettercodeblocks
 import android.annotation.SuppressLint
 import android.view.View
 import android.widget.TextView
+import android.widget.LinearLayout
 import androidx.core.content.res.ResourcesCompat
 import com.aliucord.Constants
 import com.aliucord.api.SettingsAPI
-import com.aliucord.fragments.SettingsPage
+import com.discord.app.AppBottomSheet
 import com.discord.views.CheckedSetting
+import com.discord.utilities.color.ColorCompat
 import com.lytefast.flexinput.R
 import io.github.greenpeacekt.acplugins.BetterCodeBlocks
 import io.github.greenpeacekt.acplugins.bettercodeblocks.LangNode
@@ -25,6 +27,7 @@ class Settings(private val settings: SettingsAPI) : AppBottomSheet() {
             isChecked = settings.getBool(key, false)
             setOnCheckedListener { settings.setBool(key, it) }
         })
+      return layout
     }
     companion object {
         fun get(settings: SettingsAPI): Boolean {
