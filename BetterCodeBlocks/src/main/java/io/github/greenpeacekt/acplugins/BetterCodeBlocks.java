46400f6b1a3b776c905be72dc6fdecc68be743bd
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-03-02 15:33:06 +0900

    a

--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
@@ -35,7 +35,7 @@ public final class BetterCodeBlocks extends Plugin {
     @Override
     public void start(Context context) throws Throwable {
         
-        Theme.loadtheme();
+        Theme.loadtheme(true);
         
         highlight = Prism4jSyntaxHighlight.create(prism4j, new Prism4jThemeDarkula());
 
@@ -88,6 +88,7 @@ public final class BetterCodeBlocks extends Plugin {
     @Override
     public void stop(Context context) {
         patcher.unpatchAll();
+        Theme.themes.clear();
     }
     
     /*public static Boolean Devop(){
--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/Theme.kt
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/Theme.kt
@@ -1,32 +1,95 @@
 package io.github.greenpeacekt.acplugins.bettercodeblocks
 
-import android.content.Context
-import android.graphics.Typeface
+import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
+import android.os.Build
+import android.renderscript.*
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.ColorUtils
-import com.aliucord.Utils
+import com.aliucord.*
 import com.aliucord.utils.ReflectUtils
-import com.lytefast.flexinput.R
+import org.json.JSONObject
 import java.io.File
+import java.io.FileNotFoundException
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
 
-private var colorToName = HashMap<Int, String>()
+import android.content.Context
+import android.graphics.Typeface
+import com.aliucord.Utils
+import com.lytefast.flexinput.R
 
-private val fonts = HashMap<Int, Typeface>()
 private val colorsByName = HashMap<String, Int>()
 private val colorsById = HashMap<Int, Int>()
 private val drawableTints = HashMap<Int, Int>()
-private val attrs = HashMap<Int, Int>()
-private val raws = HashMap<Int, File>()
 
 object Theme{
+    val themes = ArrayList<Theme>()
     
-    fun getNameByColor(color: Int) = colorToName[color]
-    fun getColorForName(name: String) = colorsByName[name]
-    fun getColorForId(id: Int) = colorsById[id]
+    var overlayAlpha = 0
+    
+
+    
+    private fun parseColor(json: JSONObject, key: String): Int {
+        val v = json.getString(key)
+        return if (v.startsWith("system_")) {
+            if (Build.VERSION.SDK_INT < 31)
+                throw UnsupportedOperationException("system_ colours are only supported on Android 12.")
+
+            try {
+                ContextCompat.getColor(
+                    Utils.appContext,
+                    ReflectUtils.getField(android.R.color::class.java, null, v) as Int
+                )
+            } catch (th: Throwable) {
+                throw IllegalArgumentException("No such color: $v")
+            }
+        } else v.toInt()
+    }
+    
+    private fun loadTheme(theme: Theme): Boolean {
+        overlayAlpha = 0
+        try {
+            if (!theme.convertIfLegacy())
+                theme.update()
+
+            val json = {
+                 "colors": {
+                 "primary_630": -16777216
+                 }
+            }
+
+            json.optJSONObject("colors")?.run {
+                if (has("brand_500"))
+                    putDrawableTint(
+                        "ic_nitro_rep",
+                        parseColor(this, "brand_500")
+                    )
+                keys().forEach {
+                    val v = parseColor(this, it)
+                    putColor(it, v)
+                    putAttr(it, v)
+                }
+            }
+
+        } catch (th: Throwable) {
+            logger.error("Failed to load theme ${theme.name}", th)
+            return false
+        }
+        return true
+    }
     
     
-    fun putColor(name: String, color: Int) {
+    internal fun putDrawableTint(name: String, color: Int) {
+        val id = Utils.getResId(name, "drawable")
+        if (id != 0)
+            drawableTints[id] = color
+        else
+            logger.warn("Unrecognised drawable $name")
+    }
+    
+    internal fun putColor(name: String, color: Int) {
         val id = Utils.getResId(name, "color")
         if (id != 0) {
             colorsById[id] = color
@@ -34,18 +97,20 @@ object Theme{
         } else {
             when (name) {
                 "statusbar", "input_background", "active_channel", "blocked_bg" -> colorsByName[name] = color
+                else -> logger.warn("Unrecognised colour $name")
             }
         }
     }
     
-    fun parseColor(key: String): Int {
-        val v = key
-        return v.toInt()
+    internal fun putAttr(name: String?, color: Int) =
+        ATTR_MAPPINGS[name]?.forEach {
+            setAttr(it, color)
+        }
+        
+    fun clean(){
+        colorsByName.clear()
+        colorsById.clear()
+        drawableTints.clear()
     }
-    
-    @JvmStatic
-    fun loadtheme(){
-        val v = parseColor("-16777216")
-        putColor("primary_630", v)
-    }
+
 }
