31062a1e8f53c4f23ae1c9ee6b7750286b9af588
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-03-05 17:40:59 +0900

    a

--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
@@ -69,7 +69,7 @@ public final class BetterCodeBlocks extends Plugin {
         patcher.patch(ColorDrawable.class.java.getDeclaredMethod("setColor", Int.class.javaPrimitiveType),
             new PreHook ( param ->{
                 var color = param.args[0];
-                var a = ResourceManager.INSTANCE.getColorReplacement(color)
+                var a = ResourceManager.INSTANCE.getColorReplacement(color);
                 param.args[0] = a;
             
             // Discord has blocked message colours HARDCODED, so this is the only way to theme it :husk:
