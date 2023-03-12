bb7cecc87dc715c36aeab60a95346ed6fb15b97f
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-03-12 21:25:09 +0900

    a

--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/Constants.kt
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/bettercodeblocks/Constants.kt
@@ -6,6 +6,155 @@ import java.util.regex.Pattern
 
 var currentTheme = ""
 
+enum class TransparencyMode(val value: Int) {
+    NONE(0),
+    CHAT(1),
+    CHAT_SETTINGS(2),
+    FULL(3);
+
+    companion object {
+        fun from(i: Int) = values().first { it.value == i }
+    }
+}
+
+const val DEFAULT_OVERLAY_ALPHA = 150
+val THEME_DIR = File(Constants.BASE_PATH, "themes")
+
+// Discord has these HARDCODED instead of using colour resource :husk:
+// Thus, compare colour in ColorDrawable.setColor to these values to replace them
+const val BLOCKED_COLOR_DARK = 0xff34373c.toInt()
+const val BLOCKED_COLOR_LIGHT = 0xfffcfcfc.toInt()
+
+// Credit for these colours to both https://github.com/Aliucord/DiscordThemer
+// and https://github.com/GangsterFox/AliuFox-themes/blob/main/ThemerDocu.md
+
+val ALLOWED_RESOURCE_DOMAINS = arrayOf(
+    "github.com",
+    "raw.githubusercontent.com",
+    "gitlab.com",
+    "cdn.discordapp.com",
+    "media.discordapp.net",
+    "i.imgur.com",
+    "i.ibb.co", // only for you, FrozenPhoton
+)
+
+val ALLOWED_RESOURCE_DOMAINS_PATTERN: Pattern by lazy {
+    val domains = ALLOWED_RESOURCE_DOMAINS.joinToString(
+        separator = "|",
+        transform = { Pattern.quote(it) }
+    )
+    Pattern.compile("^https://(www)?($domains)/")
+}
+
+val THEME_KEYS = arrayOf(
+    "manifest",
+    "background",
+    "fonts",
+    "raws",
+    "simple_colors",
+    "colors",
+    "drawable_tints"
+)
+
+val SIMPLE_KEYS = arrayOf(
+    "accent",
+    "background",
+    "background_secondary",
+    "mention_highlight",
+    "active_channel",
+    "statusbar",
+    "input_background",
+    "blocked_bg"
+)
+
+val SIMPLE_ACCENT_NAMES = arrayOf(
+    "link",
+    "link_light",
+    "brand_new",
+    "brand_new_230",
+    "brand_new_360", // cursor
+    "brand_new_500",
+    "brand_new_530",
+    "brand_new_560", // reactions
+    "brand_new_600",
+    // =========== Buttons ============
+    "uikit_btn_bg_color_selector_brand",
+    "uikit_btn_bg_color_selector_secondary_dark",
+    "uikit_btn_compound_color_selector_dark",
+    "uikit_btn_compound_color_selector_light",
+)
+
+val SIMPLE_BG_NAMES = arrayOf(
+    "dark_grey_2",
+    "primary_600",
+    "primary_660",
+    "primary_800",
+    "primary_dark_600",
+    "primary_dark_630",
+    "primary_dark_800"
+)
+
+val SIMPLE_BG_SECONDARY_NAMES = arrayOf(
+    "primary_500",
+    "primary_630",
+    "primary_700",
+    "primary_dark_660",
+    "primary_dark_700",
+    "input_background",
+    "statusbar",
+    "active_channel",
+)
+
+
+val SIMPLE_ACCENT_ATTRS = arrayOf(
+    "color_brand",
+    "brand_new_500",
+    "colorControlBrandForeground",
+    "colorControlActivated",
+    "colorTextLink",
+    "__alpha_10_theme_chat_mention_background",
+    "theme_chat_mention_foreground"
+)
+
+val SIMPLE_BG_ATTRS = arrayOf(
+    "colorSurface",
+    "colorBackgroundFloating",
+    "colorTabsBackground",
+    "theme_chat_spoiler_inapp_bg",
+    "primary_600",
+    "primary_660",
+    "primary_800"
+)
+
+val SIMPLE_BG_SECONDARY_ATTRS = arrayOf(
+    "colorBackgroundTertiary",
+    "colorBackgroundSecondary",
+    "primary_700",
+    "theme_chat_spoiler_bg",
+)
+
+val SIMPLE_SOUND_NAMES = arrayOf(
+    "call_calling",
+    "call_ringing",
+    "deafen",
+    "mute",
+    "undeafen",
+    "unmute",
+    "reconnect",
+    "stream_ended",
+    "stream_started",
+    "stream_user_joined",
+    "stream_user_left",
+    "user_join",
+    "user_leave",
+    "user_moved"
+)
+
+inline fun <reified T> pairOf(v: T) = v to v
+inline fun <reified T> pairOf(a: T, b: T) = Pair(a, b)
+
+val ATTR_MAPPINGS = HashMap<String, Array<String>>()
+
 fun initAttrMappings() {
     ATTR_MAPPINGS.clear()
     val map = mapOf(
