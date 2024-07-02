e211ddef4015c055984dda991ee2453617c83416
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2024-07-02 15:10:31 +0900

    update Prism_ansi.java

--- BetterCodeBlocks/src/main/java/io/noties/prism4j/languages/Prism_ansi.java
+++ BetterCodeBlocks/src/main/java/io/noties/prism4j/languages/Prism_ansi.java
@@ -15,15 +15,15 @@ public class Prism_ansi {
     @NonNull
     public static Prism4j.Grammar create(@NonNull Prism4j prism4j) {
         return grammar("ansi",
-        token("30-m", pattern(compile("\u001B\\[0;30.*?\u001B\\[0;|\\u001B\\[0;30.*", MULTILINE))),
-        token("31-m", pattern(compile("\u001B\\[0;31.*?\u001B\\[0;|\\u001B\\[0;31.*", MULTILINE))),
-        token("32-m", pattern(compile("\u001B\\[0;32.*?\u001B\\[0;|\\u001B\\[0;32.*", MULTILINE))),
-        token("33-m", pattern(compile("\u001B\\[0;33.*?\u001B\\[0;|\\u001B\\[0;33.*", MULTILINE))),
-        token("34-m", pattern(compile("\u001B\\[0;34.*?\u001B\\[0;|\\u001B\\[0;34.*", MULTILINE))),
-        token("35-m", pattern(compile("\u001B\\[0;35.*?\u001B\\[0;|\\u001B\\[0;35.*", MULTILINE))),
-        token("36-m", pattern(compile("\u001B\\[0;36.*?\u001B\\[0;|\\u001B\\[0;36.*", MULTILINE))),
-        token("37-m", pattern(compile("\u001B\\[0;37.*?\u001B\\[0;|\\u001B\\[0;37.*", MULTILINE))),
-        token("38-m", pattern(compile("\u001B\\[0;38.*?\u001B\\[0;|\\u001B\\[0;38.*", MULTILINE)))
+        token("30-m", pattern(compile("\[0;30m.*?\[|\[0;30m.*", MULTILINE))),
+        token("31-m", pattern(compile("\[0;31m.*?\[|\[0;31m.*", MULTILINE))),
+        token("32-m", pattern(compile("\[0;32m.*?\[|\[0;32m.*", MULTILINE))),
+        token("33-m", pattern(compile("\[0;33m.*?\[|\[0;33m.*", MULTILINE))),
+        token("34-m", pattern(compile("\[0;34m.*?\[|\[0;34m.*", MULTILINE))),
+        token("35-m", pattern(compile("\[0;35m.*?\[|\[0;35m.*", MULTILINE))),
+        token("36-m", pattern(compile("\[0;36m.*?\[|\[0;36m.*", MULTILINE))),
+        token("37-m", pattern(compile("\[0;37m.*?\[|\[0;37m.*", MULTILINE))),
+        token("38-m", pattern(compile("\[0;38m.*?\[|\[0;38m.*", MULTILINE)))
      );
     }
 }
