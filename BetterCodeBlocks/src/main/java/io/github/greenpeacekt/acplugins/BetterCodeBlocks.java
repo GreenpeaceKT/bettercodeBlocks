01a0f7ea355c26ec509e03c44ec4ca33f65bc7d0
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2024-07-03 12:21:26 +0900

    update BetterCodeBlocks.java

--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
@@ -124,7 +124,6 @@ public final class BetterCodeBlocks extends Plugin {
                     String matched = matcher.group(1);
                     content = matched.replaceAll("\\[\\d+;\\d+m", "");
                 }
-                entry.getMessage().getContent() = content;
             }
         ));
     }
