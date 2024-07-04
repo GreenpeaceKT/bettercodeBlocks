f4b612b0be2e53a43d53c5fe2dca999540686bca
GreenpeaceKT <68542998+GreenpeaceKT@users.noreply.github.com>
2024-07-04 09:50:52 +0900

    update BetterCodeBlocks.java

--- BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
+++ BetterCodeBlocks/src/main/java/io/github/greenpeacekt/acplugins/BetterCodeBlocks.java
@@ -127,8 +127,8 @@ public final class BetterCodeBlocks extends Plugin {
                         Logger.debug(mes);
                         mes = matched.replaceAll("\\[\\d+;\\d+m", "");
                         ReflectUtils.setField(content, "textContent", mes);
+                        Logger.debug("text");
                     }
-                
                     ReflectUtils.setField(content, "textContent", mes);
                 } catch (Exception e) {
                     e.printStackTrace();
